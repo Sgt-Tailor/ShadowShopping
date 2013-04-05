bd7ab1f82585c500e767680e4a0683331f58adac
Sgt_Tailor <sven.wiltink@hotmail.com>
2013-04-05 12:02:57 -0700

    fixed import bullshit

--- src/com/shadowblox/sw123/ShadowShopping/commands/ShadowShoppingCommand.java
+++ src/com/shadowblox/sw123/ShadowShopping/commands/ShadowShoppingCommand.java
@@ -2,10 +2,12 @@ package com.shadowblox.sw123.ShadowShopping.commands;
 
 import org.bukkit.command.CommandSender;
 
+import com.shadowblox.sw123.ShadowShopping.korikutils.SubCommandExecutor;
 
 
 
-public class ShadowShoppingCommand extends com.shadowblox.sw123.ShadowShopping.korikutils.SubCommandExecutor{
+
+public class ShadowShoppingCommand extends SubCommandExecutor{
 	
 	@command
 	public void shop(CommandSender sender, String[] args){//shadowshopper shop create 
