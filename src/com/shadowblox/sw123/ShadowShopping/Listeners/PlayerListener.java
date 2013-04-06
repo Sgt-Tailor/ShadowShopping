80a3a6d289384f956e966824da03bca09f2a574f
Sgt_Tailor <sven.wiltink@hotmail.com>
2013-04-06 02:56:19 -0700

    Added more checking methods

--- src/com/shadowblox/sw123/ShadowShopping/Listeners/PlayerListener.java
+++ src/com/shadowblox/sw123/ShadowShopping/Listeners/PlayerListener.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 import com.shadowblox.sw123.ShadowShopping.ShadowShopping;
@@ -21,6 +22,9 @@ public class PlayerListener implements Listener {
 			p.openInventory(shop.getInventory());
 			event.setCancelled(true);
 		}
+	}
+	@EventHandler onPlayerBuyItem(InventoryClickEvent event){
+		
 	}
 
 }
--- src/com/shadowblox/sw123/ShadowShopping/Managers/PlayerManager.java
+++ src/com/shadowblox/sw123/ShadowShopping/Managers/PlayerManager.java
@@ -1,11 +1,40 @@
 package com.shadowblox.sw123.ShadowShopping.Managers;
 
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
-public class PlayerManager{
+public class PlayerManager {
 
-	public static boolean playerCanAfford(Player p, int price){
-		//do weird vault shit
+	public static boolean playerCanAfford(Player p, int price) {
+		// do weird vault shit
 		return true;
+	}
+
+	public static boolean playerHasSpace(Player p, ItemStack item, int amount) {
+		Inventory pInv = p.getInventory();
+		int space = 0;
+		for (ItemStack i : pInv.getContents()) {
+			if (i != null) {
+				if (i.getMaxStackSize() > 1) {
+					if (i.getTypeId() == item.getTypeId()) {
+						if (i.getData().equals(item.getData())) {
+							if(i.getItemMeta().equals(item.getItemMeta())){//the item should be the same
+								int free = i.getMaxStackSize() - i.getAmount();
+								space = space + free;
+							}
+						}
+					}
+				}
+			} else {
+				space = space + item.getMaxStackSize();
+			}
+		}
+		if(space >= amount){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 }
